0a9122a00b7b5aeb307f597961627107970faf87
Amka <Aminhassen1470@gmail.com>
2024-02-04 22:25:46 -0500

    install zod and update ts-config

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
