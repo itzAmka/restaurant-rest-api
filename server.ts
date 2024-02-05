2733e5259e3718a7ac6f7e8657021356daa8e211
Amka <Aminhassen1470@gmail.com>
2024-02-04 23:23:37 -0500

    Add error middleware and handle 404 errors

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 
+// middlewares
+import { errorMiddleware } from './middlewares/errorMiddleware';
+
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 const app: Express = express();
 
@@ -13,6 +16,17 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
 	res.json({ message: 'Welcome to the API' });
 });
+
+// handle 404 errors
+app.use('*', (req, res) => {
+	res.status(404).json({
+		error: 'Not found ',
+		message: `Can't find ${req.originalUrl} on this server!`,
+	});
+});
+
+// error middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, () =>
 	console.log(`Server running at http://localhost:${PORT}`),
