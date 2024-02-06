c0ff89a44fd2cef7c1761c36ff6dec7ae060427c
Amka <Aminhassen1470@gmail.com>
2024-02-06 11:45:25 -0500

    add admin login data schema

--- utils/schema/admin/auth-schema.ts
+++ utils/schema/admin/auth-schema.ts
@@ -28,3 +28,12 @@ export const adminRegisterDataSchema = z.object({
 });
 
 export type TAdminRegisterationData = z.infer<typeof adminRegisterDataSchema>;
+
+export const adminLoginDataSchema = z.object({
+	email: z
+		.string({ required_error: 'Email is required!' })
+		.email({ message: 'Please provide a valid email address' }),
+	password: z.string({ required_error: 'Password is required!' }),
+});
+
+export type TAdminLoginData = z.infer<typeof adminLoginDataSchema>;
