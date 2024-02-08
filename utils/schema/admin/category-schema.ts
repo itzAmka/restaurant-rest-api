3525ad14303caa45d845009374b3612f90864000
Amin <Aminhassen85@gmail.com>
2024-02-08 13:07:47 -0500

    update `categorySchema` in category-schema.ts

--- utils/schema/admin/category-schema.ts
+++ utils/schema/admin/category-schema.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 
 export const categorySchema = z.object({
-	name: z.string(),
-	description: z.string(),
+	name: z.string({
+		required_error: 'Please provide `name`',
+		invalid_type_error: '`name` must be a string',
+	}),
+	description: z.string({
+		required_error: 'Please provide `description`',
+		invalid_type_error: '`description` must be a string',
+	}),
 });
 
 export type TCategoryData = z.infer<typeof categorySchema>;
