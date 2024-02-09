db17e7c79ab66afda34b02fa056950b0f1654190
Amin <Aminhassen85@gmail.com>
2024-02-08 23:42:55 -0500

    create `getAllMenuController`

---
+++ controllers/menu-controllers.ts
@@ -0,0 +1,47 @@
+import asyncHandler from 'express-async-handler';
+
+import {
+	createMenuService,
+	getAllMenuService,
+	getMenuByIdService,
+	updateMenuService,
+	deleteMenuService,
+} from '../services/admin/menu-services';
+import ServerError from '../utils/server-error';
+
+// Get all menu controller
+export const getAllMenuController = asyncHandler(async (req, res) => {
+	const { searchTerm } = req.query as { searchTerm: string };
+
+	// pagination
+	const page = parseInt((req.query.page as string) ?? 1);
+	const limit = parseInt((req.query.limit as string) ?? 10);
+	const skip = (page - 1) * limit;
+
+	const results = await getAllMenuService(searchTerm, {
+		limit,
+		skip,
+	});
+
+	const totalPages = Math.ceil(results.totalCount / limit);
+	const nextPage = page < totalPages ? page + 1 : null;
+	const prevPage = page > 1 ? page - 1 : null;
+
+	res.status(200).json({
+		success: true,
+		error: null,
+		results: {
+			data: {
+				menu: results.menu,
+			},
+			pagination: {
+				limit,
+				page,
+				totalPages,
+				prevPage,
+				nextPage,
+				totalCount: results.totalCount,
+			},
+		},
+	});
+});
