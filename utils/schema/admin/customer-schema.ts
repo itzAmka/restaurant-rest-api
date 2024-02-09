e94d6489516250c5a7a536f5580ce0fdf2f63b1a
Amin <Aminhassen85@gmail.com>
2024-02-09 11:18:32 -0500

    add  phone number transformation to correct the format

--- utils/schema/admin/customer-schema.ts
+++ utils/schema/admin/customer-schema.ts
@@ -39,20 +39,42 @@ export const customerSchema = z.object({
 					}
 
 					// convert to correct format
-					const formattedPhone = val.replace(
-						/(\d{3})(\d{3})(\d{4})/,
-						'$1-$2-$3',
-					);
+					val = val.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
 
-					return formattedPhone;
+					return val;
 				}
 
 				return val;
 			},
 			{
-				message: 'Phone number must be in the format `555-555-5555`',
+				message:
+					'Phone number must be in the format `555-555-5555` and 10 digits long',
 			},
-		),
+		)
+		.transform((val) => {
+			// pattern for phone number 555-555-5555
+			const pattern = /^\d{3}-\d{3}-\d{4}$/;
+
+			if (!pattern.test(val)) {
+				// trim spaces
+				val = val.trim();
+
+				// remove all non-digits characters
+				val = val.replace(/\D/g, '');
+
+				// make sure it's 10 digits
+				if (val.length !== 10) {
+					return false;
+				}
+
+				// convert to correct format
+				val = val.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+
+				return val;
+			}
+
+			return val;
+		}),
 });
 
 export type TCustomer = z.infer<typeof customerSchema>;
