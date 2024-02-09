d57ee9e49bf4fbddb7d26fa79ffac35063ce4550
Amin <Aminhassen85@gmail.com>
2024-02-09 17:26:02 -0500

    create `getAllCustomersController` to retrieve all customers with pagination

--- controllers/customer-controllers.ts
+++ controllers/customer-controllers.ts
@@ -36,3 +36,40 @@ export const createCustomerController = asyncHandler(async (req, res) => {
 		},
 	});
 });
+
+// Get all customers controller
+export const getAllCustomersController = asyncHandler(async (req, res) => {
+	const { searchTerm } = req.query as { searchTerm: string };
+
+	// pagination
+	const page = parseInt((req.query.page as string) ?? 1);
+	const limit = parseInt((req.query.limit as string) ?? 10);
+	const skip = (page - 1) * limit;
+
+	const results = await getAllCustomersService(searchTerm, {
+		limit,
+		skip,
+	});
+
+	const totalPages = Math.ceil(results.totalCount / limit);
+	const nextPage = page < totalPages ? page + 1 : null;
+	const prevPage = page > 1 ? page - 1 : null;
+
+	res.status(200).json({
+		success: true,
+		error: null,
+		results: {
+			data: {
+				customers: results.customers,
+			},
+			pagination: {
+				limit,
+				page,
+				totalPages,
+				prevPage,
+				nextPage,
+				totalCount: results.totalCount,
+			},
+		},
+	});
+});
