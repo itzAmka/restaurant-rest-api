6da705f54a7ac98022badbf16d83db3c9bbc66b0
Amin <Aminhassen85@gmail.com>
2024-02-08 23:45:24 -0500

    create `createMenuController`

--- controllers/menu-controllers.ts
+++ controllers/menu-controllers.ts
@@ -9,6 +9,42 @@ import {
 } from '../services/admin/menu-services';
 import ServerError from '../utils/server-error';
 
+// Create menu controller
+export const createMenuController = asyncHandler(async (req, res) => {
+	const { name, price, description, categoryId } = req.body;
+
+	// Validate data
+	if (
+		!name ||
+		!price ||
+		description === undefined ||
+		description === null ||
+		!categoryId
+	) {
+		throw new ServerError(
+			400,
+			`All fields are required: \`name\`, \`price\`, \`description\`, \`categoryId\``,
+		);
+	}
+
+	const newMenu = await createMenuService({
+		name,
+		price,
+		description,
+		categoryId,
+	});
+
+	res.status(200).json({
+		success: true,
+		error: null,
+		results: {
+			data: {
+				newMenu,
+			},
+		},
+	});
+});
+
 // Get all menu controller
 export const getAllMenuController = asyncHandler(async (req, res) => {
 	const { searchTerm } = req.query as { searchTerm: string };
